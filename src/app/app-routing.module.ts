ded072377969462735fa354bdca5584f5d3d657f
shpp-vhrebenko <vhrebenko@gmail.com>
2016-12-06 20:29:51 +0200

    edit: app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,20 +7,12 @@ import { ParettoEditComponent } from './paretto-edit/paretto-edit.component.ts';
 import { ParettoAdminComponent } from './paretto-admin/paretto-admin.component.ts';
 
 const routes: Routes = [
-<<<<<<< HEAD
   { path: 'parreto',  component:  ParretoMainComponent},
   { path: 'parreto/list', component: ParretoListComponent},
   { path: 'parreto/edit/:id', component: ParretoEditComponent },
   { path: 'parreto/admin', component: ParretoAdminComponent },
   { path: '', redirectTo: '/parreto', pathMatch: 'full' },
   /*{ path: '**', component: NotFoundComponent }*/
-=======
-  { path: '', redirectTo: '/paretto', pathMatch: 'full' },
-  { path: 'paretto',  component:  ParettoMainComponent},
-  { path: 'paretto/list', component: ParettoListComponent},
-  { path: 'paretto/edit/:id', component: ParettoEditComponent },
-  { path: 'paretto/admin', component: ParettoAdminComponent }
->>>>>>> 25a5046a60d8a1de600d239fb149d6c1425d7ed1
 ];
 
 @NgModule({
